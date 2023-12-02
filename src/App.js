67e1c1c6b58b614c68729fd897c56d218d2db6c9
Sushmita <sushmitakumari363@gmail.com>
2023-12-02 17:51:20 +0530

    card created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import { user } from "./Data/user";
+import { UsersCard } from "./Components/UsersCard";
 
 function App() {
+  const [users_data, setUsers_data] = useState(user);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users_data.map((user) => (
+        <UsersCard user={user} key={user.id} />
+      ))}
     </div>
   );
 }
---
+++ src/Components/UsersCard.jsx
@@ -0,0 +1,41 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+export const UsersCard = ({ user }) => {
+  return (
+    <Card
+      style={{
+        width: 250,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <CardMedia
+        sx={{ height: 100, width: 50 }}
+        image={user.avatar}
+        title="avatar"
+        component="img"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Name: {user.first_name} {user.last_name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Email: {user.email}
+          <br />
+          Gender:{user.gender} <br />
+          Domain: {user.domain}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {user.available && <Button size="small">Add to Team</Button>}
+      </CardActions>
+    </Card>
+  );
+};
